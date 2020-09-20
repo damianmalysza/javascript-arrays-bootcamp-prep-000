62204422be56ec85410001d79439d6a7ab6f18b3
Damian Malysza <damian.malysza@gmail.com>
2020-09-20 13:33:57 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -23,3 +23,14 @@ function destructivelyAddElementToEndOfArray(array,element){
 function accessElementInArray(array,index){
   return array[index]
 }
+
+function destructivelyRemoveElementFromBeginningOfArray(array){
+  array.shift()
+  return array
+}
+
+function removeElementFromBeginningOfArray(array){
+  var newArray=array.slice(1)
+  return newArray
+}
+
