7d52d023665fda247fb0392115b0a4923e7e6317
Damian Malysza <damian.malysza@gmail.com>
2020-09-20 13:23:40 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,2 +1,11 @@
 var chocolateBars=["snickers","hundred grand","kitkat","skittles"]
 
+function addElementToBeginningOfArray(array,element){
+  return array.unshift(element)
+}
+
+function destructivelyAddElementToBeginningOfArray(array,element){
+  array[0]=element
+  return array
+}
+
